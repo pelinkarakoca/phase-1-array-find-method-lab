6d6126140b9e98848bb6cbd8454e11db10e5fb6b
Pelin Karakoca <karakocapelin@gmail.com>
2022-12-13 13:26:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+
+function superbowlWin(record) {
+  const isWin = record.find((element) => element.result === "W");
+  console.log(isWin);
+  return isWin ? isWin.year : undefined;
+}
+
+superbowlWin(record);
